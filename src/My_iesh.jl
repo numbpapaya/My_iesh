e66a02fd81010c39bf6af22a5001b798323ff615
Belal Raza <belal.kevin.raza@gmail.com>
2021-03-04 20:48:09 +0100

    first testing

--- src/My_iesh.jl
+++ src/My_iesh.jl
@@ -1,5 +1,10 @@
 module My_iesh
 
 # Write your package code here.
+greet() = print("Hello World")
+
+include("extra_file.jl")
+export f
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using My_iesh
 using Test
 
+
 @testset "My_iesh.jl" begin
     # Write your tests here.
+    @test f(2, 1) == 7
+    @test f(2, 3) == 13
 end
